96bb293cb135a6a28abf4dc59c9f02020e1cc04a
Sergio Susa <sergio@sergiosusa.com>
2019-02-28 23:55:31 +0100

    * Add Chrono.GG Auto flip a coin.

---
+++ games/chronogg-flip-a-coin.user.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name         ChronoGG Flip a Coin
+// @namespace    http://sergiosusa.com/
+// @version      0.1
+// @description  ChronoGG Auto Flip a Coin
+// @author       Sergio Susa (sergio@sergiosusa.com)
+// @match        https://chrono.gg
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+    let chronoGG = new ChronoGG(0.01);
+    chronoGG.flipACoin();
+})();
+
+function ChronoGG(hoursToReload) {
+
+    this.hoursToReload = hoursToReload;
+
+    this.flipACoin = () => {
+        document.getElementById('reward-coin').click();
+        jQuery().click();
+        this.reloadPage(this.hoursToReload)
+    };
+
+    this.reloadPage = (hours) => {
+        setTimeout(function () {
+            window.location.reload();
+        }, hours * 60 * 60 * 1000);
+    };
+}
