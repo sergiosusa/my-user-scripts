bf6d1cbe2bb850b1f93c4e3a7c11bfd72f6369ff
Sergio Susa <sergio@sergiosusa.com>
2020-05-10 15:38:21 +0200

    Update indiegala-bundle-exposer.user.js
    update to work with the new IndieGala web version.

--- games/indiegala-bundle-exposer.user.js
+++ games/indiegala-bundle-exposer.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Indiegala Bundles' Exposer
 // @namespace    https://sergiosusa.com
-// @version      0.4
+// @version      0.5
 // @description  Reveal and copy each game of a indiegala's bundle.
 // @author       Sergio Susa (sergio@sergiosusa.com)
-// @match        https://www.indiegala.com/gift?gift_id=*
+// @match        https://www.indiegala.com/gift-bundle/*
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
 // @grant GM_setValue
 // @grant GM_getValue
@@ -55,11 +55,17 @@ function IndieGalaExposer() {
         })
     };
 
+    this.revealAndCopyExcel = () => {
+        that.reveal().then(() => {
+            that.copyToExcel();
+        })
+    };
+
     this.reveal = () => {
 
         return new Promise((resolve) => {
 
-            let revealButtons = jQuery('a.create-permalink-btn');
+            let revealButtons = document.querySelectorAll("div.profile-private-page-library-serial-dialog > div > button.profile-private-page-library-get-serial-btn.bg-gradient-blue");
             let x = 0;
 
             let revealIntervalId = setInterval(() => {
@@ -72,13 +78,13 @@ function IndieGalaExposer() {
                 revealButtons[x].click();
                 x++;
 
-            }, 2000);
+            }, 4000)
         });
     };
 
     this.copyToExcel = () => {
-        let titles = document.querySelectorAll('h4.title_game');
-        let serials = document.querySelectorAll('input.keys');
+        let titles = document.querySelectorAll('div.profile-private-page-library-title-row-full');
+        let serials = document.querySelectorAll('input.profile-private-page-library-key-serial');
         let list = [];
 
         for(let x = 0; x < titles.length; x++) {
@@ -92,7 +98,7 @@ function IndieGalaExposer() {
     this.copy = () => {
 
         let serials = [];
-        let inputSerials = jQuery('input.keys');
+        let inputSerials = jQuery('input.profile-private-page-library-key-serial');
 
         for (let x = 0; x < inputSerials.length; x++) {
             serials.push(inputSerials[x].value);
@@ -104,30 +110,43 @@ function IndieGalaExposer() {
 }
 
 function insertGraphicElements(revealer) {
-    let buttonContainer = jQuery('#indie_gala_2 > div')[0];
-    let anchor = document.createElement('a');
-    anchor.innerHTML = '( Reveal and Copy )';
-    anchor.href = '#';
-    anchor.style.color='#EC2028';
-    anchor.style.fontWeight = 'bold';
-    anchor.onclick = revealer.revealAndCopy;
-    buttonContainer.append(anchor);
 
-    let copyAnchor = document.createElement('a');
-    copyAnchor.innerHTML = '( Copy )';
-    copyAnchor.href = '#';
-    copyAnchor.style.color='#EC2028';
-    copyAnchor.style.fontWeight = 'bold';
-    copyAnchor.onclick = revealer.copy;
-    buttonContainer.append(copyAnchor);
+    let manuContainer = document.querySelectorAll("nav.profile-private-page-library-menu > ul")[0];
 
-    let copyExcelAnchor = document.createElement('a');
-    copyExcelAnchor.innerHTML = '( Copy To Excel )';
-    copyExcelAnchor.href = '#';
-    copyExcelAnchor.style.color='#EC2028';
-    copyExcelAnchor.style.fontWeight = 'bold';
-    copyExcelAnchor.onclick = revealer.copyToExcel;
-    buttonContainer.append(copyExcelAnchor);
+    let emptyOption = document.createElement('li');
+    emptyOption.innerHTML = '<div class="profile-private-page-library-menu-item-inner"><a href="#" onclick="return false"></a></div>';
+    manuContainer.appendChild(emptyOption);
+
+    let revealAndCopyOption = document.createElement('li');
+    revealAndCopyOption.style.padding = "0 1px";
+    revealAndCopyOption.innerHTML = '<li class="active bg-gradient-red"><div class="profile-private-page-library-menu-item-inner"><a id="revealCopybtn" href="#" onclick="return false">Reveal + Copy</a></div></li>';
+    manuContainer.appendChild(revealAndCopyOption);
+
+    let copyOption = document.createElement('li');
+    copyOption.style.padding = "0 1px";
+    copyOption.innerHTML = '<li class="active bg-gradient-red"><div class="profile-private-page-library-menu-item-inner"><a id="copybtn" href="#" onclick="return false">Copy</a></div></li>';
+    manuContainer.appendChild(copyOption);
+
+    let revealAndCopyExcelOption = document.createElement('li');
+    revealAndCopyExcelOption.style.padding = "0 1px";
+    revealAndCopyExcelOption.innerHTML = '<li class="active bg-gradient-red"><div class="profile-private-page-library-menu-item-inner"><a id="revealCopyExcelbtn" href="#" onclick="return false">Reveal + Copy Excel</a></div></li>';
+    manuContainer.appendChild(revealAndCopyExcelOption);
+
+    let copyExcelOption = document.createElement('li');
+    copyExcelOption.style.padding = "0 1px";
+    copyExcelOption.innerHTML = '<li class="active bg-gradient-red"><div class="profile-private-page-library-menu-item-inner"><a id="copyExcelbtn" href="#" onclick="return false">Copy Excel</a></div></li>';
+    manuContainer.appendChild(copyExcelOption);
+
+
+    let revealCopybtn= document.getElementById('revealCopybtn');
+    revealCopybtn.onclick = revealer.revealAndCopy;
+    let copybtn= document.getElementById('copybtn');
+    copybtn.onclick = revealer.copy;
+    let revealCopyExcelbtn= document.getElementById('revealCopyExcelbtn');
+    revealCopyExcelbtn.onclick = revealer.revealAndCopyExcel
+    let copyExcelbtn= document.getElementById('copyExcelbtn');
+    copyExcelbtn.onclick = revealer.copyToExcel;
+
 }
 
 /***********************************************************
