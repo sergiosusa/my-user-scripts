6100d68835290220346da01a28261ea98870ca79
Sergio Susa <sergio@sergiosusa.com>
2020-06-05 22:32:11 +0200

    Update steam-own-game-bundle-checker.user.js
    Compare for exactMatch after use the js library.

--- games/steam-own-game-bundle-checker.user.js
+++ games/steam-own-game-bundle-checker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Steam Own Game Bundle Checker
 // @namespace    https://sergiosusa.com/
-// @version      0.3
+// @version      0.4
 // @description  Check against your games library if you have already got the game (sites support: humblebundle, indiegala, fanatical, bunchkeys).
 // @author       Sergio Susa (sergio@sergiosusa.com)
 // @match        https://www.bunchkeys.com/*
@@ -9,22 +9,23 @@
 // @match        https://www.indiegala.com/*
 // @match        https://www.fanatical.com/*/bundle/*
 // @match        https://otakubundle.com/*
-// @grant        none
+// @grant        GM_xmlhttpRequest
 // @require https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
+// @require https://cdnjs.cloudflare.com/ajax/libs/elasticlunr/0.9.6/elasticlunr.js
 // ==/UserScript==
 
-const STEAM_API = '';
-const STEAM_USER_ID = '76561198041196449';
+const STEAM_USER = '76561198041196449';
+const STEAM_API_ID = '';
 
 (function () {
     'use strict';
     $.noConflict();
 
-    let steamApi = new SteamAPI(STEAM_API);
+    let steamApi = new SteamAPI();
     let checkerFactory = new CheckerFactory();
     let checker = checkerFactory.createChecker(getDomainFromCurrentUrl());
 
-    steamApi.getOwnedGames(STEAM_USER_ID).then(
+    steamApi.getOwnedGames(STEAM_USER, STEAM_API_ID).then(
         (games) => {
             checker.check(games);
         }
@@ -64,18 +65,23 @@ function Checker() {
     this.compareGames = (games, myGames) => {
 
         for (let x = 0; x < games.length; x++) {
-            let found = false;
-            for (let y = 0; y < myGames.game_count; y++) {
-                if (myGames.games[y].name.trim().toLowerCase() === games[x].innerText.replace('Locked content','').trim().toLowerCase()) {
-                    this.own.push(games[x]);
-                    found = true;
-                }
-            }
-            if (!found) {
+
+            let gameName = games[x].innerText.replace('Locked content', '').replace('Product details', '').replace('Detalles del producto').trim();
+            let results = myGames.search(gameName);
+
+            if (results.length > 0 && this.exactMatch(results[0].doc.name, gameName))
+            {
+                this.own.push(games[x]);
+
+            } else {
                 this.notOwn.push(games[x]);
             }
         }
-    }
+    };
+
+    this.exactMatch = (resultGame, myGame) => {
+        return resultGame.toLowerCase() === myGame.toLowerCase();
+    };
 }
 
 function HumbleBundle() {
@@ -133,7 +139,7 @@ function IndieGala() {
     };
 
     this.getHtmlSpanResult = (color, text) => {
-        return '<span style="color:' + color + ';margin-left:0;font-weight: bold;background:none;display:inline;">(' + text + ')</span>';
+        return '<span style="color:' + color + ';margin-left:0;font-weight: bold;background:none;display:inline;margin-left: 17px;">(' + text + ')</span>';
     }
 }
 
@@ -228,30 +234,43 @@ function OtakuBundle() {
 
 OtakuBundle.prototype = Object.create(Checker.prototype);
 
-function SteamAPI(steamApiKey) {
-    const OWN_GAMES_ENDPOINT = "https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=[STEAM-API-ID]&steamid=[STEAM-USER-ID]&include_appinfo=1";
+function SteamAPI() {
+    const OWN_GAMES_ENDPOINT = "https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=[STEAM_API_ID]&steamid=[STEAM_USER]&include_appinfo=true";
 
-    this.steamApiKey = steamApiKey;
+    this.getOwnedGames = (steamUser, steamApiId) => {
 
-    this.getOwnedGames = (steamUserId) => {
-        let that = this;
 
-        return new Promise((resolve, reject) => {
+        return new Promise(((resolve, reject) => {
+            GM_xmlhttpRequest({
+                method: "GET",
+                url: OWN_GAMES_ENDPOINT.replace('[STEAM_USER]', steamUser).replace("[STEAM_API_ID]", steamApiId),
+                onload: function (response) {
 
-            jQuery.ajax({
-                url: OWN_GAMES_ENDPOINT.replace('[STEAM-API-ID]', that.steamApiKey).replace('[STEAM-USER-ID]', steamUserId),
-                dataType: 'JSONP',
-                type: 'GET',
-                jsonp: 'jsonp',
-                success: (data) => {
-                    that.storeGames(steamUserId, data.response);
-                    resolve(that.retrieveGames(steamUserId));
-                },
-                error: () => {
-                    reject(null);
-                }
+                    elasticlunr.addStopWords(['™']);
+
+                    var index = elasticlunr(function () {
+                        this.addField('name');
+                        this.setRef('appId');
+                    });
+
+                    let games = JSON.parse(response.responseText).response.games;
+
+                    for (let x = 0; x < games.length; x++) {
+                        let appId = games[x].appid;
+                        let name = games[x].name.trim();
+
+                        let doc = {
+                            "appId": appId,
+                            "name": name
+                        };
+
+                        index.addDoc(doc);
+                    }
+                    this.storeGames(steamUser, index);
+                    resolve(index);
+                }.bind(this)
             });
-        });
+        }).bind(this));
     };
 
     this.storeGames = (steamUserId, response) => {
@@ -260,9 +279,6 @@ function SteamAPI(steamApiKey) {
 
     this.retrieveGames = steamUserId => JSON.parse(localStorage.getItem(steamUserId))
 }
-
-
-
 
 function getDomainFromCurrentUrl() {
     let matches = window.location.href.match(/https?:\/\/(www\.)?([-a-zA-Z0-9@:%._+~#=]{2,256})(\.[a-z]{2,6})/i);
